7453bad9ca7b8adc1be1206a33dcc60d71916048
SebastianHajdus <sebastian.hajdus@gmail.com>
2017-03-08 00:08:13 +0100

    working on responsive nav bar

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ $(function () {
 
 console.log('dziala');
 
+var $navigation = $('.nav-list');
 
+$('.hamburger-menu').on('click',function() {
+    $navigation.toggleClass('is-active');
+});
 
 
 });
